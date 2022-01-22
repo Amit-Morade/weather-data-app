fbc076715f9d093adcee0514cdbe7bd16a236714
Amit Morade <65406995+Amit-Morade@users.noreply.github.com>
2022-01-22 10:40:04 +0530

    added the templates directory and updated app.js with all necessary settings to use views, partials

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
+const { rmSync } = require('fs')
 
 const pathToPublic = path.join(__dirname, "../public")
+const pathToViews = path.join(__dirname, "../templates/views")
+const pathToPartials = path.join(__dirname, "../templates/partials")
+
 const port = process.env.PORT || 3000
 const app = express()
+app.set("view engine", "hbs")
+app.set("views", pathToViews)
+hbs.registerPartials(pathToPartials)
 
 app.use(express.static(pathToPublic))
+
+//setup few routes
+//about page 
+//help page
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
 app.listen(port, () => {
     console.log("server is up and running")
