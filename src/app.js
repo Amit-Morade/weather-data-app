3faa9b260360b024c7f93b3f6e3ccfc10bdd35f2
Amit Morade <65406995+Amit-Morade@users.noreply.github.com>
2022-01-22 10:19:05 +0530

    added new features to index.html and styles.css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const path = require('path')
 
+const pathToPublic = path.join(__dirname, "../public")
 const port = process.env.PORT || 3000
 const app = express()
+
+app.use(express.static(pathToPublic))
 
 app.listen(port, () => {
     console.log("server is up and running")
