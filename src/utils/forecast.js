f4dcbf6f1da54f1882ea8dcbceea4f9710da9af3
Amit Morade <amitmorade19@gmail.com>
2023-02-27 08:39:24 +0530

    api key changed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
 
-    const url = 'http://api.weatherstack.com/current?access_key=5394567b5accf0924a8a055f4f2dfff1&query=' + latitude + ',' + longitude;
+    const url = 'http://api.weatherstack.com/current?access_key=e6068d154836879ce1757075e151c680&query=' + latitude + ',' + longitude;
 
     request({url, json: true}, (error, response) => {
         if(error){
